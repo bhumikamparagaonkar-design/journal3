8644f0f5589665a5595a9de806263e85c7d41e85
bhumikamparagaonkar-design <bhumikamparagaonkar@gmail.com>
2025-11-15 10:04:38 +0530

    Update score.py

--- score.py
+++ score.py
@@ -1,18 +1,14 @@
 import sys
 
-# If arguments are passed, use them
-if len(sys.argv) > 1:
-    scores = list(map(int, sys.argv[1:]))
-else:
-    # If no arguments passed (optional fallback)
-    scores = list(map(int, input("Enter scores separated by space: ").split()))
+if len(sys.argv) < 2:
+    print("Usage: python score.py 10 20 30 40")
+    sys.exit(1)
+
+scores = list(map(int, sys.argv[1:]))
 
 total = sum(scores)
 average = total / len(scores)
-maximum = max(scores)
-minimum = min(scores)
 
-print("Sum of scores:", total)
-print("Average of scores:", average)
-print("Maximum score:", maximum)
-print("Minimum score:", minimum)
+print("Scores:", scores)
+print("Sum:", total)
+print("Average:", average)
